f0ce18d49ed26b76795469f8813eaa60a4fec373
Засолкин Евгений <men6586@yandex.ru>
2022-09-09 14:26:23 +0300

    first commit

---
+++ first.py
@@ -0,0 +1,29 @@
+name = 'Evgenii'
+age = 26
+spisok = (123, name, 'asd') # не изменямый
+list_example = [123, age, name]
+number = 213.879
+list_example[0]
+list_example.append(1)
+
+example_dict = {'key': 'value'}
+example_dict['key']
+
+
+for i in range(10):
+    print('asd')
+
+
+for elemnt in list_example:
+    print(elemnt)
+
+x = 10
+while x > 0:
+    print('dsa')
+    x -= 1
+if 5 > 4:
+    print('qwe')
+elif 1 > 6:
+    print('njn')
+else:
+    print('uy')
